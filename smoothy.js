dfd36c5416fce31715b857afa4a2b1e003396e60
Aalok T <thapa.aalok@gmail.com>
2017-02-07 13:38:09 +0800

    formatting code

--- smoothy.js
+++ smoothy.js
@@ -1,122 +1,122 @@
 ;(function($, window){
 
-	var defaults = {
+  var defaults = {
 
-		// whether to scroll to elements based on hash
-		onLoad: true,
+    // whether to scroll to elements based on hash
+    onLoad: true,
 
-		// offset to apply to when scrolling to target elements
-		offset: 0,
+    // offset to apply to when scrolling to target elements
+    offset: 0,
 
-		// the selector used to bind elements to smooth scroll
-		selector: 'a[data-href]',
+    // the selector used to bind elements to smooth scroll
+    selector: 'a[data-href]',
 
-		// function to run before starting the scroll,
-		// receives the scrollType: 'hash' or 'click' and
-		// an optional trigger when scroll behavior is invoked via a click
-		beforeScroll: function( scrollType, trigger ){}
-	};
+    // function to run before starting the scroll,
+    // receives the scrollType: 'hash' or 'click' and
+    // an optional trigger when scroll behavior is invoked via a click
+    beforeScroll: function( scrollType, trigger ){}
+  };
 
-	var Smoothy = function( elements, options ){
-		this.elements = elements;
-		this.options = $.extend( {}, defaults, options );
-		this.init();
-	};
+  var Smoothy = function( elements, options ){
+    this.elements = elements;
+    this.options = $.extend( {}, defaults, options );
+    this.init();
+  };
 
-	Smoothy.prototype = {
+  Smoothy.prototype = {
 
-		_getById: function( id ){
-			return $(id);
-		},
+    _getById: function( id ){
+      return $(id);
+    },
 
-		init: function(){
+    init: function(){
 
-			var p = this,
-				o = p.options
-			;
+      var p = this,
+      o = p.options
+        ;
 
-			if( o.onLoad ){
-				p.scrollByHash();
-			}
+      if( o.onLoad ){
+        p.scrollByHash();
+      }
 
-			p.elements.each(function(){
-				$(o.selector).on('click', function( e ){
-					p.scrollByClick( $(this), e );
-				});	
-			});			
-		},
+      p.elements.each(function(){
+        $(o.selector).on('click', function( e ){
+          p.scrollByClick( $(this), e );
+        });
+      });
+    },
 
-		scrollByHash: function(){
+    scrollByHash: function(){
 
-			var p = this,
-				o = p.options,
-				hash = window.location.hash
-			;
+      var p = this,
+      o = p.options,
+      hash = window.location.hash
+        ;
 
-			if( !hash.length ) return;
+      if( !hash.length ) return;
 
-			var $target = p._getById( hash );
-			if( !$target.length ) return;
+      var $target = p._getById( hash );
+      if( !$target.length ) return;
 
-			$('html,body').hide();
+      $('html,body').hide();
 
-			setTimeout( function(){
+      setTimeout( function(){
 
-				$('html,body').scrollTop(0).show();
+        $('html,body').scrollTop(0).show();
 
-				if( typeof o.beforeScroll === 'function' ){
-					o.beforeScroll( 'hash' );
-				}
-				
-				p.scrollTo( $target );
+        if( typeof o.beforeScroll === 'function' ){
+          o.beforeScroll( 'hash' );
+        }
 
-			}, 0);
-		},
+        p.scrollTo( $target );
 
-		scrollByClick: function( $trigger, e ){
+      }, 0);
+    },
 
-			var p = this,
-				o = p.options,
-				hash = $trigger.attr('data-href'),
-				$target = p._getById( hash )
-			;
+    scrollByClick: function( $trigger, e ){
 
-			if( !$target.length ) return;
+      var p = this,
+      o = p.options,
+      hash = $trigger.attr('data-href'),
+      $target = p._getById( hash )
+        ;
 
-			e.preventDefault();
+      if( !$target.length ) return;
 
-			if( typeof o.beforeScroll === 'function' ){
-				o.beforeScroll( 'click', $trigger );
-			}
+      e.preventDefault();
 
-			p.scrollTo( $target );
-		},
+      if( typeof o.beforeScroll === 'function' ){
+        o.beforeScroll( 'click', $trigger );
+      }
 
-		scrollTo: function( $target ){
+      p.scrollTo( $target );
+    },
 
-			var p = this,
-			o = p.options
-			;
+    scrollTo: function( $target ){
 
-			if( !$target.length ) return;
+      var p = this,
+      o = p.options
+        ;
 
-			
-			$('body').animate({
-				scrollTop: $target.offset().top - p.options.offset
-			});
+      if( !$target.length ) return;
 
-		}
-	};
 
-	$.fn.smoothy = function( options ){
+      $('body').animate({
+        scrollTop: $target.offset().top - p.options.offset
+      });
 
-		var smoothy = new Smoothy( this, options ),
-			$html = $('html')
-		;
+    }
+  };
 
-		if( !$.data( $html, 'smoothy-loaded' ) ){
-			$.data( $html, 'smoothy-loaded', smoothy );
-		}
-	}
+  $.fn.smoothy = function( options ){
+
+    var smoothy = new Smoothy( this, options ),
+    $html = $('html')
+      ;
+
+    if( !$.data( $html, 'smoothy-loaded' ) ){
+      $.data( $html, 'smoothy-loaded', smoothy );
+    }
+  }
 
 })(jQuery, window);
